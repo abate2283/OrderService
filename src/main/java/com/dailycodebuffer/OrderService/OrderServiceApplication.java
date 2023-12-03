73a25527801a6b53ad752c6c59dc55a90f32cffb
abate2283 <alfredbate@yahoo.co.uk>
2023-12-03 05:07:49 -0600

    new code changes

--- src/main/java/com/dailycodebuffer/OrderService/OrderServiceApplication.java
+++ src/main/java/com/dailycodebuffer/OrderService/OrderServiceApplication.java
@@ -2,7 +2,16 @@ package com.dailycodebuffer.OrderService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,4 +21,25 @@ public class OrderServiceApplication {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
+@Bean
+@LoadBalanced
+public RestTemplate restTemplate(){return new RestTemplate();}
+
+//@Bean
+//public OAuth2AuthorizedClientManager clientManager(
+//		ClientRegistrationRepository clientRegistrationRepository,
+//		OAuth2AuthorizedClientRepository auth2AuthorizedClientRepository
+//){
+//	OAuth2AuthorizedClientProvider oAuth2AuthorizedClientProvider
+//			= OAuth2AuthorizedClientProviderBuilder
+//			.builder()
+//			.clientCredentials()
+//			.build();
+//	DefaultOAuth2AuthorizedClientManager oAuth2AuthorizedClientManager
+//			= new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository, auth2AuthorizedClientRepository);
+//	oAuth2AuthorizedClientManager.setAuthorizedClientProvider(oAuth2AuthorizedClientProvider);
+//	return oAuth2AuthorizedClientManager;
+//}
+
 }
+
